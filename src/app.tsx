b9192a232e4fae7ebdcff6f786402b24f244ad6c
Hendrew <hendrewneresfsa@hotmail.com>
2024-03-11 12:03:16 -0300

    commit

---
+++ src/app.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import logo from './assets/logo-nlw.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
+
+export function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    setNotes([newNote, ...notes])
+  }
+
+  return ( 
+    <div className="mx-auto max-w-6xl my-12 space-y-6 ">
+      <img src={logo} alt='NLW EXPERT' />
+      
+      <form className="w-full">
+
+      <input
+       type="text" placeholder='busque em suas notas...' 
+       className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+       
+       />
+      </form>
+
+     <div className="h-px bg-slate-700"/>
+
+     <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+    
+      <NewNoteCard onNoteCreated={onNoteCreated} />
+
+
+      {notes.map(note => {
+        return <NoteCard key={note.id} note={note} />
+      
+      })}
+     </div>
+    </div>
+  )
+}
---
+++ src/components/new-note-card.tsx
@@ -0,0 +1,86 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { X } from 'lucide-react'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { toast } from 'sonner'
+
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void
+}
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps){
+  const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true)
+  const [content, setContent] = useState('')
+
+  function handleStartEditor(){
+    setShouldShowOnBoarding(false)
+  }
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+    setContent(event.target.value)
+
+    if (event.target.value == '') {
+      setShouldShowOnBoarding(true)
+  }
+}
+
+  function handleSaveNote(event:  FormEvent) {
+    event.preventDefault()
+
+    onNoteCreated(content)
+
+    setContent('')  
+
+    toast.success('Nota salva com sucesso!')
+  }
+    return (
+
+    
+        <Dialog.Root>
+        
+        <Dialog.Trigger className="rounded-md flex flex-col  bg-slate-700 text-left p-5 gap-3 outline-none space-y-3 hover:ring-2 hover:ring-slate-600 focus-visible::ring-2 focus-visible:ring-lime-400">
+          <span className="text-sm font-medium text-slate-200">
+            Adicionar nota           
+          </span>
+            <p className="text-sm leading-6 text-slate-400">
+              Grave uma nota em áudio que será convertida para texto automaticamente.
+            </p>
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+                <Dialog.Overlay className="inset-0 fixed bg-black/50"/>
+                <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+                  <form></form>
+                    <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+                        <X className="size-5" />
+                    </Dialog.Close>
+
+                    <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
+                          <div className="flex flex-1 flex-col gap-3 p-5">
+                              <span className="text-sm font-medium text-slate-300">
+                                Adicionar nota
+                              </span>
+
+                              {shouldShowOnBoarding ? (
+                                <p className="text-sm leading-6 text-slate-400">
+                                Comece <button className="font-medium hover:underline text-lime-400">gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className="font-medium hover:underline text-lime-400">utilize apenas texto</button>.
+                              </p>
+                              ) : (
+                                <textarea 
+                                  autoFocus
+                                  className="text-sm leading-6 text-slate-400 bg-transparent flex-1 outline-none"
+                                  onChange={handleContentChanged}                          
+                                  />
+                                )}
+                          </div>
+
+                          <button
+                              type="submit"
+                              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+                          >
+                          Salvar nota
+                          </button>
+                        </form>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
+    )
+  }
